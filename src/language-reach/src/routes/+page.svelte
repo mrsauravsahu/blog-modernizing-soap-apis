6d04fece5db98154d558b5f80cbc4c1c25f8a73f
Saurav Sahu <mrsauravsahu@outlook.com>
2023-08-14 23:43:44 +0530

    improv: update styling for the app with tailwind

--- src/language-reach/src/routes/+page.svelte
+++ src/language-reach/src/routes/+page.svelte
@@ -17,7 +17,6 @@
   });
 
   const filterFunction: ComboboxFilterFunction<string> = (item, inputValue) => {
-    // Example string normalization function. Replace as needed.
     const normalize = (str: string) => str.normalize().toLowerCase();
     const normalizedInput = normalize(inputValue);
     return (
@@ -49,70 +48,69 @@
   });
 </script>
 
-<h1>language reach</h1>
+<main class="p-8 grid gap-4">
+  <h1 class="text-4xl text-center">Language Reach</h1>
 
-<p>where can a language be useful?</p>
+  <p class="text-xl text-center p-4">Where can a language be useful?</p>
 
-<div class="flex flex-col gap-1">
-  <!-- svelte-ignore a11y-label-has-associated-control - $label contains the 'for' attribute -->
-
-  <div class="relative">
-    <input
-      use:melt={$input}
-      class="flex h-10 items-center justify-between rounded-md bg-white
-            px-3 pr-12 text-magnum-700"
-      placeholder="English"
-    />
-  </div>
-</div>
-{#if $open}
-  <ul
-    class="z-10 flex max-h-[300px] flex-col overflow-hidden rounded-md"
-    use:melt={$menu}
-    transition:fly={{ duration: 150, y: -5 }}
-  >
-    <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-    <div
-      class="flex max-h-full flex-col gap-2 overflow-y-auto bg-white px-2 py-2"
-      tabindex="0"
-    >
-      {#each $filteredItems as language, index (index)}
-        <li
-          use:melt={$item({
-            index,
-            item: language,
-          })}
-          class="relative cursor-pointer rounded-md py-1 pl-8 pr-4 text-neutral-800
-                          data-[highlighted]:bg-magnum-100 data-[highlighted]:text-magnum-700
-                          data-[disabled]:opacity-50"
-        >
-          <div>
-            <span class="block text-sm opacity-70">{language}</span>
-          </div>
-        </li>
-      {/each}
+  <div class="flex flex-col gap-1 items-center"> <!-- Center the search field -->
+    <div class="relative">
+      <input
+        use:melt={$input}
+        class="flex h-10 items-center justify-between rounded-md bg-white
+          px-3 pr-12"
+        placeholder="English"
+      />
     </div>
-  </ul>
-{/if}
+  </div>
 
-<ul>
-  {#each validCountries as country (country.name)}
-    <li>
-      <p>{country.name}</p>
-      <img src={country.countryFlagUrl} alt={`Flag of ${country.name}`} />
-      <h3>Languages:</h3>
-      <ul>
-        {#each country.languageDetails.languages as language (language.name)}
-          <li>{language.name}</li>
+  {#if $open}
+    <ul
+      class="z-10 max-h-[300px] overflow-y-auto bg-white rounded-md shadow-lg"
+      use:melt={$menu}
+      transition:fly={{ duration: 150, y: -5 }}
+    >
+      <div class="flex flex-col gap-2 p-2">
+        {#each $filteredItems as language, index (index)}
+          <li
+            use:melt={$item({
+              index,
+              item: language,
+            })}
+            class="relative cursor-pointer rounded-md py-1 pl-8 pr-4 text-neutral-800
+              data-[highlighted]:bg-magnum-100
+              data-[disabled]:opacity-50"
+          >
+            <span class="block text-sm opacity-70">{language}</span>
+          </li>
         {/each}
-      </ul>
-    </li>
-  {/each}
-</ul>
+      </div>
+    </ul>
+  {/if}
+
+  <ul class="grid grid-cols-1 md:grid-cols-3 gap-4"> <!-- Display three tiles in one line on larger screens -->
+    {#each validCountries as country (country.name)}
+      <li class="border rounded-lg overflow-hidden">
+        <figure class="md:flex p-2 md:p-4 space-x-4">
+          <div class="flex-grow">
+            <p class="text-lg font-semibold">{country.name}</p>
+            <p class="text-sm text-gray-600">
+              {country.languageDetails.languages.map(l => l.name).join(", ")}
+            </p>
+          </div>
+          <img
+            class="w-48 h-30 rounded-xl"
+            src={country.countryFlagUrl}
+            alt={`Flag of ${country.name}`}
+          />
+        </figure>
+      </li>
+    {/each}
+  </ul>
+</main>
 
 <style lang="postcss">
-  .check {
-    @apply absolute left-2 top-1/2 text-magnum-500;
-    translate: 0 calc(-50% + 1px);
+  :global(html) {
+    background-color: theme(colors.gray.100);
   }
 </style>
